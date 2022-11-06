eb48ffcb6dad2f9103cd40f9d05c25f910506503
Jacob Drath <89097881+jacobd082@users.noreply.github.com>
2022-11-06 14:16:20 -0600

    Small Update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+
+
+/*
 const FeatureList = [
   {
     title: 'Easy to Use',
@@ -62,3 +65,5 @@ export default function HomepageFeatures() {
     </section>
   );
 }
+
+*/
