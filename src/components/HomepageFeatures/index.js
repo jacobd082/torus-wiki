886952de6463ed277d54fdff934df99e7fe25793
Jacob Drath <89097881+jacobd082@users.noreply.github.com>
2022-11-06 14:17:55 -0600

    Fix

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,8 +4,10 @@ import styles from './styles.module.css';
 
 
 
-/*
+
 const FeatureList = [
+  /*
+
   {
     title: 'Easy to Use',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
@@ -16,26 +18,7 @@ const FeatureList = [
       </>
     ),
   },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  */
 ];
 
 function Feature({Svg, title, description}) {
@@ -66,4 +49,3 @@ export default function HomepageFeatures() {
   );
 }
 
-*/
